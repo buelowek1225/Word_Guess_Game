5fcf37427ab174473e22fe1b32c51bde99eb9679
Ellen Buelow <buelowek1225@gmail.com>
2019-04-27 09:31:51 -0500

    html outline and javascript basic functions

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,1 +1,19 @@
-console.log("test");
+// list of unique geographic regions or locations
+let geolocations = [
+    "machu picchu", 
+    "rocky mountains", 
+    "sahara desert", 
+    "mauna loa", 
+    "angel water falls"
+]
+// console.log(geolocations);
+
+//randomly pick a value from my array
+var rand = geolocations[Math.floor(Math.random() * geolocations.length)];
+// console.log(rand);
+
+// key listener, waits for key to be pressed and then reports the key name
+document.addEventListener("keydown",function(e){
+});
+console.log(e.key);
+
