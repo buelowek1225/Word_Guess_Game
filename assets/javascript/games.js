19f98a60f951fbcd7ac9b30a005322d4fa33a404
Ellen Buelow <buelowek1225@gmail.com>
2019-04-28 11:25:49 -0500

    Guesses Left and Letters Used-added

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -51,6 +51,12 @@ var geolocations = [
 ]
 // console.log(geolocations);
 
+//Wrong Letter Array
+var wrongletters = [];
+
+//Guesses left
+var guesses = 12;
+
 //randomly pick a value from my array
 var rand = Math.floor(Math.random() * geolocations.length);
 // console.log(rand);
@@ -83,18 +89,43 @@ document.getElementById("test1").textContent = geoword.join("")
 // key listener, waits for key to be pressed and then reports the key name
 // in the document add an event listener for any key pressed, run this function(defined a), when you console.log it will print the event, Key is translating that key pressed to the value given by the keyboard
 document.addEventListener("keydown", function(keyevent){
+    var match = false;
     for (let i = 0; i < geolocations[rand].Letters.length; i++){
         if (geolocations[rand].Letters[i] === keyevent.key) {
-            console.log(keyevent.key);
+            match = true;
+            // console.log(keyevent.key);
             // assign keyevent.key to element in "geoword";
             geoword[i] = keyevent.key;
-            console.log(geoword);
+            // console.log(geoword.join(""));
         }
     }
-
     
-//     // console.log(geoword)
-//     // console.log(keyevent.key);
+    //converts '_, _, _' array to string
+    document.getElementById("test1").textContent = geoword.join("")
+    //make a var to set up array for wrong letters 
+
+    if (match === false){
+        console.log (keyevent.key)
+        
+    //Print keyevent under 'letters used', add element to array to keep all elements printed
+        wrongletters.push(keyevent.key);
+        // console.log (wrongletters)
+        document.getElementById("test2").textContent = wrongletters
+    //Decrease Guesses left by '1'
+        if (guesses > 0 ) {
+            guesses -= 1
+        }
+        if (guesses == 0 ) {
+            document.getElementById("losemessage").textContent = "You Lose :( "
+        }
+        document.getElementById("test3").textContent = guesses
+            
+  
+      
+    }
+
+
+
+
 })
 
-// take key pressed and comparing it to the geolocation[rand][i] elements
